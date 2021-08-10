b7818e87a9983ead7a4cf9a11ebb64c1339a394e
Synthesizer-source <56120358+Synthesizer-source@users.noreply.github.com>
2021-08-10 16:11:03 +0300

    add keyboard event listener library and improve search view animation

--- app/src/main/java/com/synthesizer/source/rawg/ui/NewHomeFragment.kt
+++ app/src/main/java/com/synthesizer/source/rawg/ui/NewHomeFragment.kt
@@ -1,16 +1,18 @@
 package com.synthesizer.source.rawg.ui
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import com.synthesizer.source.rawg.R
 import com.synthesizer.source.rawg.databinding.FragmentNewHomeBinding
+import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEvent
 
 class NewHomeFragment : Fragment() {
 
@@ -18,17 +20,22 @@ class NewHomeFragment : Fragment() {
     private val binding get() = _binding!!
     private var enabled = true
     private var onBackPressedCallback: OnBackPressedCallback? = null
+    private var inputManager: InputMethodManager? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentNewHomeBinding.inflate(inflater, container, false)
+        inputManager =
+            requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
         onBackPressedCallback = object :
             OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
+                this.remove()
                 if (!enabled) {
-                    this.remove()
                     requireActivity().onBackPressed()
                 }
                 backMotion()
@@ -41,9 +48,12 @@ class NewHomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        KeyboardVisibilityEvent.setEventListener(requireActivity(), viewLifecycleOwner, {
+            if (!it) backMotion()
+        })
+
         binding.edittextfield.setOnFocusChangeListener { view, isFocused ->
             if (view.isInTouchMode && isFocused) {
-                Log.d("synthesizer-source", "backMotion: clicked! $enabled")
                 view.performClick()
             }
         }
