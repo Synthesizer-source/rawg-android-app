b15554aaa69b602a92d7c4aac3ed419c9a9184c3
Synthesizer-source <56120358+Synthesizer-source@users.noreply.github.com>
2021-08-11 10:57:32 +0300

    fix transition bug

--- app/src/main/java/com/synthesizer/source/rawg/ui/NewHomeFragment.kt
+++ app/src/main/java/com/synthesizer/source/rawg/ui/NewHomeFragment.kt
@@ -1,16 +1,19 @@
 package com.synthesizer.source.rawg.ui
 
+import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
+import android.text.InputType
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import androidx.activity.OnBackPressedCallback
+import androidx.core.animation.doOnEnd
+import androidx.core.animation.doOnStart
 import androidx.fragment.app.Fragment
-import com.synthesizer.source.rawg.R
 import com.synthesizer.source.rawg.databinding.FragmentNewHomeBinding
 import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEvent
 
@@ -18,9 +21,9 @@ class NewHomeFragment : Fragment() {
 
     private var _binding: FragmentNewHomeBinding? = null
     private val binding get() = _binding!!
-    private var enabled = true
+    private var isExpand = true
     private var onBackPressedCallback: OnBackPressedCallback? = null
-    private var inputManager: InputMethodManager? = null
+    private var inputMethodManager: InputMethodManager? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,29 +31,35 @@ class NewHomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentNewHomeBinding.inflate(inflater, container, false)
-        inputManager =
+        inputMethodManager =
             requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        registerListeners()
+        return binding.root
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun registerListeners() {
+
+        KeyboardVisibilityEvent.setEventListener(requireActivity(), viewLifecycleOwner, {
+            if (!it && !isExpand) transitionToStart()
+        })
 
         onBackPressedCallback = object :
             OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 this.remove()
-                if (!enabled) {
+                if (isExpand) {
                     requireActivity().onBackPressed()
+                } else {
+                    transitionToStart()
                 }
-                backMotion()
             }
         }
-        return binding.root
-    }
 
-    @SuppressLint("ClickableViewAccessibility")
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        KeyboardVisibilityEvent.setEventListener(requireActivity(), viewLifecycleOwner, {
-            if (!it) backMotion()
-        })
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            onBackPressedCallback!!
+        )
 
         binding.edittextfield.setOnFocusChangeListener { view, isFocused ->
             if (view.isInTouchMode && isFocused) {
@@ -60,28 +69,65 @@ class NewHomeFragment : Fragment() {
 
         binding.edittextfield.setOnTouchListener(object : View.OnTouchListener {
             override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-                searchViewOnClick()
+                onSearchViewClick()
                 return false
             }
         })
     }
 
-    private fun searchViewOnClick() {
-        enabled = true
-        binding.constraintLayout.enableTransition(R.id.clickSearch, true)
-        binding.constraintLayout.transitionToEnd()
-        addBackButtonCallback()
+    private fun onSearchViewClick() {
+        if (binding.root.scrollY != 0) scrollToTop() else transitionToEnd()
     }
 
-    private fun addBackButtonCallback() {
-        requireActivity().onBackPressedDispatcher.addCallback(
-            viewLifecycleOwner,
-            onBackPressedCallback!!
-        )
+    private fun scrollToTop() {
+        val duration = 500L
+        val valueAnimator: ValueAnimator =
+            ValueAnimator.ofInt(binding.root.scrollY, binding.root.top)
+        valueAnimator.duration = duration
+        valueAnimator.addUpdateListener {
+            val value = it.animatedValue as Int
+            binding.root.scrollTo(0, value)
+        }
+
+        valueAnimator.doOnStart {
+            hideKeyboard()
+        }
+
+        valueAnimator.doOnEnd {
+            showKeyboard()
+            transitionToEnd()
+            it.removeAllListeners()
+        }
+
+        valueAnimator.start()
     }
 
-    private fun backMotion() {
-        enabled = false
+    private fun transitionToStart() {
+        binding.visitWebSiteButton.isEnabled = true
         binding.constraintLayout.transitionToStart()
+        isExpand = true
+    }
+
+    private fun transitionToEnd() {
+        binding.visitWebSiteButton.isEnabled = false
+        binding.constraintLayout.transitionToEnd()
+        isExpand = false
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        onBackPressedCallback = null
+        inputMethodManager = null
+    }
+
+    private fun showKeyboard() {
+        binding.edittextfield.inputType = InputType.TYPE_CLASS_TEXT
+        binding.edittextfield.requestFocus()
+        inputMethodManager!!.showSoftInput(binding.edittextfield, InputMethodManager.SHOW_FORCED)
+    }
+
+    private fun hideKeyboard() {
+        binding.edittextfield.clearFocus()
+        binding.edittextfield.setRawInputType(InputType.TYPE_NULL)
     }
 }
